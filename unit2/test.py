a1d543c1c7c347e2684a44271c55468c745b3f71
Jibin Liu <liujibin320@gmail.com>
2018-04-09 20:29:51 -0700

    add tests for pipeline

--- unit2/test.py
+++ unit2/test.py
@@ -1,17 +1,51 @@
-
-def rot13_transform(s):
-    # do transform for aphabetic leters
-    char_list = list(s)
-
-    for index, char in enumerate(char_list):
-        ord_num = ord(char)
-        if ord_num >= 65 and ord_num <= 90:  # lowercase
-            char_list[index] = chr(ord_num + 13) if ord_num <= 77 else chr(ord_num - 13)
-        elif ord_num >= 97 and ord_num <= 122:  # uppercase
-            char_list[index] = chr(ord_num + 13) if ord_num <= 109 else chr(ord_num - 13)
-
-    res = ''.join(char_list)
-    return res
+from rot13 import Rot13
+from sign_up import SignUp
+import unittest
 
 
-print(rot13_transform('abc'))
+class TestRot13(unittest.TestCase):
+    def test_rot13(self):
+        rot = Rot13()
+        self.assertEqual(rot.rot13_transform('abc'), 'nop')
+        self.assertEqual(rot.rot13_transform('    '), '    ')
+        self.assertEqual(rot.rot13_transform('#$%^'), '#$%^')
+
+
+class TestSignUp(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.signup = SignUp()
+
+    def testValidateUsername(self):
+        self.assertTrue(self.signup.valid_username('dfdsjiuoew'))
+        self.assertTrue(self.signup.valid_username('dye7210843'))
+
+        self.assertFalse(self.signup.valid_username('#$%^%#ds'))
+        self.assertFalse(self.signup.valid_username('djfdu923*&'))
+        self.assertFalse(self.signup.valid_username('  '))
+        self.assertFalse(self.signup.valid_username(''))
+        self.assertFalse(self.signup.valid_username('aa'))
+        self.assertFalse(self.signup.valid_username('a' * 21))
+
+    def testValidatePassword(self):
+        self.assertTrue(self.signup.valid_password('*(&(*&jkdu'))
+        self.assertTrue(self.signup.valid_password('            '))
+        self.assertTrue(self.signup.valid_password('80382jkldsa'))
+
+        self.assertFalse(self.signup.valid_username('  '))
+        self.assertFalse(self.signup.valid_username(''))
+        self.assertFalse(self.signup.valid_username('ab'))
+        self.assertFalse(self.signup.valid_username('a' * 21))
+
+    def testValidateEmail(self):
+        self.assertTrue(self.signup.valid_email('abc@gma.com'))
+        self.assertTrue(self.signup.valid_email('190283@aol.net'))
+
+        self.assertFalse(self.signup.valid_username('aa@gmail'))
+        self.assertFalse(self.signup.valid_username('@aol.com'))
+        self.assertFalse(self.signup.valid_username('gdjuieow.com'))
+        self.assertFalse(self.signup.valid_username(''))
+
+if __name__ == '__main__':
+    unittest.main()
